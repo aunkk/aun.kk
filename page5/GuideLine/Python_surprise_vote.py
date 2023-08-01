75410528daad7e9e2349b6be222915e1eb5a0285
aun.kk <121716689+aunkk@users.noreply.github.com>
2023-08-01 18:35:54 +0700

    Delete page5 directory

--- page5/GuideLine/Python_surprise_vote.py
+++
@@ -1,9 +0,0 @@
-"""asd"""
-def trial(total, max_number):
-    """asd"""
-    second = min((total-max_number), max_number)
-    min_number = total - (max_number + second)
-    print("Surprising" * (max_number - min_number > 2) + "Not surprising" * (max_number - min_number <= 2))
-    print("%d, %d, %d" % (max_number, second, min_number))
-
-trial(float(input()), float(input()))
--- page5/Python_Triangle.py
+++
@@ -1,3 +0,0 @@
-"""Triangle"""
-def main():
-    """"""
