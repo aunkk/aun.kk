44ed3759c22167e4bf38db992414e9776d383af8
aun.kk <121716689+aunkk@users.noreply.github.com>
2023-08-01 18:35:37 +0700

    Delete page4 directory

--- page4/Python_ODD_EVEN.py
+++
@@ -1,7 +0,0 @@
-"""Odd-Even"""
-def is_odd():
-    """Check number is odd or not"""
-    number = int(input())
-    print(("False" * (number%2 == 0)) + ("True" * (number%2 != 0)))
-
-is_odd()
--- page4/Python_robot.py
+++
@@ -1,9 +0,0 @@
-"""Robot I"""
-def main():
-    """output and condition"""
-    name = str(input())
-    age = float(input())
-    print((("%s, you can pass." % name) * (age < 18)) + \
-        (("%s, you shall not pass." % name) * (age >= 18)))
-
-main()
